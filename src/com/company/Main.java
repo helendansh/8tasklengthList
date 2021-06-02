e59ae1e0d06a6fdd96c4a57855a9db5d6636efba
helendansh <danshinahel@yandex.ru>
2021-06-02 15:48:04 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> stringlist = new ArrayList<>();
+        for (int i=0; i<10; i++) {
+            stringlist.add(reader.readLine());
+        }
+        for (int i=0; i<stringlist.size(); i++) {
+            if (stringlist.get(i).length() > stringlist.get(i+1).length()) {
+                System.out.println(i+1); break;
+            }
+        }
+    }
+}
